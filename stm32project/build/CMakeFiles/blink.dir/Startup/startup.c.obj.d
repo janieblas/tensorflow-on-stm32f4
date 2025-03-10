2df71ef00e7b75d6e80fbc50ff36cf9b59431f18
anieblas <nibj961116@gmail.com>
2025-03-09 20:38:16 -0600

    new branch to add the tensorflow drivers

--- stm32project/build/CMakeFiles/blink.dir/Startup/startup.c.obj.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/blink.dir/Startup/startup.c.obj: \
- /home/janieblas/Documents/repositories/stm32f446_repo/e006_hal_driver/Startup/startup.c \
- /usr/share/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
- /usr/share/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/stdint.h \
- /usr/share/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
- /usr/share/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/features.h \
- /usr/share/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
- /usr/share/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
- /usr/share/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h
